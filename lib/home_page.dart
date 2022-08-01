4c17b97ebd4e0748c6b897049f538cd793c92f14
Roronoa786Zoro <aniket199mistry@gmail.com>
2022-08-01 19:26:50 +0530

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "aniket";
+
+    return Scaffold(
+      //in scaffold we have many components
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome  $name  to a $days days of Flutter tutorial "),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
