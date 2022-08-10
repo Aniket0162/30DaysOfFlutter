da84ede2e1bcd92b53971a86e741b147ac3956df
Roronoa786Zoro <aniket199mistry@gmail.com>
2022-08-10 20:29:06 +0530

    day9 completed

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,5 +1,4 @@
-import 'dart:html';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -9,24 +8,59 @@ class MyDrawer extends StatelessWidget {
         "https://pbs.twimg.com/profile_images/1553712988280000513/cI2VKVUb_400x400.jpg";
     // final imageUrl = "Zoro.jpg";
     return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-            padding: EdgeInsets.zero,
-            child: UserAccountsDrawerHeader(
-              margin: EdgeInsets.zero,
-              decoration: BoxDecoration(color: Colors.red),
-              accountName: Text("Aniket Mistry"),
-              accountEmail: Text("aniket199mistry@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(imageUrl),
-              ),
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                decoration: BoxDecoration(color: Colors.deepPurple),
+                accountName: Text("Aniket Mistry"),
+                accountEmail: Text("aniket199mistry@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
 
-              // if you want too use downloaded image then use this
-              // currentAccountPicture: Image.asset("assets/images/Zoro.jpg"),
+                // if you want too use downloaded image then use this
+                // currentAccountPicture: Image.asset("assets/images/Zoro.jpg"),
+              ),
             ),
-          )
-        ],
+            ListTile(
+              leading: Icon(CupertinoIcons.home, color: Colors.white),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading:
+                  Icon(CupertinoIcons.profile_circled, color: Colors.white),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(CupertinoIcons.mail, color: Colors.white),
+              title: Text(
+                "E-mail me",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
